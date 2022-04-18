7d20a8dc59be87acf7e8e2f596ad1a2f0ee970f5
MichaelCampbell2020 <cam3000@gmail.com>
2022-04-18 11:59:47 -0700

    bt template

---
+++ command-handling/commands/createCommand.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('createcommand')
+		.setDescription('Creates an API access command.')
+    .addStringOption(option =>
+      option.setName('REST-method')
+        .setDescription('The REST method to use for this API call.')
+        .setRequired(true)
+        .addChoice('Read', 'method_read')
+        .addChoice('Update', 'method_update')
+        .addChoice('Delete', 'method_delete')
+        .addChoice('Create', 'method_create')
+    )
+    .addStringOption(option =>
+      option.setName('API-URL')
+        .setDescription('API endpoint URL.')
+        .setRequired(true)
+    ),
+	async execute(interaction) {
+
+		return interaction.reply('Pong!');
+	},
+};
