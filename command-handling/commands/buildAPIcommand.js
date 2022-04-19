47f01b5072078f2b0672df92ce0bf0510756a509
Micha Davis <m.leigh.davis@gmail.com>
2022-04-18 17:14:02 -0700

    fix add choice bug

--- command-handling/commands/buildAPIcommand.js
+++ command-handling/commands/buildAPIcommand.js
@@ -1,26 +1,22 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('createcommand')
-		.setDescription('Creates an API access command.')
+  data: new SlashCommandBuilder()
+    .setName('createcommand')
+    .setDescription('Creates an API access command.')
     .addStringOption(option =>
-      {console.log(option)
       option.setName('rest-method')
         .setDescription('The REST method to use for this API call.')
         .setRequired(true)
-        .addChoice('Read', 'method_read')
-        .addChoice('Update', 'method_update')
-        .addChoice('Delete', 'method_delete')
-        .addChoice('Create', 'method_create')
-      })
+        .addChoices({ name: 'Read', value: 'method_read' }, { name: 'Update', value: 'method_update' }, { name: 'Delete', value: 'method_delete' }, { name: 'Create', value: 'method_create' })
+    )
     .addStringOption(option =>
-      option.setName('API-URL')
+      option.setName('api-url')
         .setDescription('API endpoint URL.')
         .setRequired(true),
     ),
-	async execute(interaction) {
-
-		return interaction.reply('Pong!');
-	},
+  async execute(interaction) {
+    const value = interaction.options.getString('rest-method');
+    return interaction.reply(`You have selected ${value}`)
+  },
 };
