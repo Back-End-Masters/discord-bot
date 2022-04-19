5c245e3fbed0cab32c64e52209ac719e4652aa7f
MichaelCampbell2020 <cam3000@gmail.com>
2022-04-19 15:12:28 -0700

    returns well-formatted results

--- command-handling/commands/buildAPIcommand.js
+++ command-handling/commands/buildAPIcommand.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const fetch = require('node-fetch');
-
+const beautify = require('json-beautify');
 //creates object for our command and allows us to execute our command (interaction)
 module.exports = {
   data: new SlashCommandBuilder()
@@ -43,12 +43,12 @@ module.exports = {
     await fetch(request)
     .then(response => response.json())
     .then(data => {
-      results = data;
+      results = beautify(data, null, 2, 100);
 
     })
     console.log(results);
     //TO DO: return code block to user from api endpoint based on their params
-    return interaction.reply(`\`\`\`${results}\`\`\``);
+    return interaction.reply(`\`\`\`json\n${results}\n\`\`\``);
   },
 };
 
