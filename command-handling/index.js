62f95fc911d145666431b46626462119285ab762
Tiara Brown <tiaradominique.b@gmail.com>
2022-04-19 19:13:30 -0400

    heroku test

--- command-handling/index.js
+++ command-handling/index.js
@@ -3,7 +3,7 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
+//leaving a comment for heroku
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
