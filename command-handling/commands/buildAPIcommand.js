a1fd61fec271cf81e5c80801a34591d022ff8b01
Micha Davis <m.leigh.davis@gmail.com>
2022-04-20 11:37:37 -0700

    remove rest method

--- command-handling/commands/buildAPIcommand.js
+++ command-handling/commands/buildAPIcommand.js
@@ -6,12 +6,12 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('createcommand')
     .setDescription('Creates an API access command.')
-    .addStringOption(option =>
-      option.setName('rest-method')
-        .setDescription('The REST method to use for this API call.')
-        .setRequired(true)
-        .addChoices({ name: 'Read', value: 'get' }, { name: 'Update', value: 'put' }, { name: 'Delete', value: 'delete' }, { name: 'Create', value: 'post' })
-    )
+    // .addStringOption(option =>
+    //   option.setName('rest-method')
+    //     .setDescription('The REST method to use for this API call.')
+    //     .setRequired(true)
+    //     .addChoices({ name: 'Read', value: 'get' }, { name: 'Update', value: 'put' }, { name: 'Delete', value: 'delete' }, { name: 'Create', value: 'post' })
+    // )
     .addStringOption(option =>
       option.setName('api-url')
         .setDescription('API endpoint URL.')
@@ -29,7 +29,7 @@ module.exports = {
     ),
   async execute(interaction) {
     console.log('INTERACTION:', interaction.options._hoistedOptions);
-    const method = interaction.options.getString('rest-method').toUpperCase();
+    //const method = interaction.options.getString('rest-method').toUpperCase();
     const url = interaction.options.getString('api-url');
     const key1 = interaction.options.getString('key-one');
     const val1 = interaction.options.getString('value-one');
@@ -38,17 +38,20 @@ module.exports = {
     if (key1) {
       request += `?${key1}=${val1}`;
     }
-    // await interaction.deferReply();
-    let results;
-    await fetch(request)
-    .then(response => response.json())
-    .then(data => {
-      results = beautify(data, null, 2, 100);
-
-    })
-    console.log(results);
-    //TO DO: return code block to user from api endpoint based on their params
-    return interaction.reply(`\`\`\`json\n${results}\n\`\`\``);
+    try {
+      let results = await fetch(request)
+        .then(response => response.json())
+        .then(data => {
+          return beautify(data, null, 2, 100);
+        })
+      console.log('results', results);
+      console.log('this is what success should look like');
+      //TO DO: return code block to user from api endpoint based on their params
+      return interaction.reply(`\`\`\`json\n${results}\n\`\`\``);
+    } catch (error) {
+      console.log('error', error);
+      throw new Error(error);
+    }
   },
 };
 
